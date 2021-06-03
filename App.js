37341d68e9d9647925608fcc0b3b1751f0523807
MD Rezaul Karim <rkrezaulkarim07@gmail.com>
2021-06-03 21:35:27 +0600

    add the stack navigator

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, React Native World!!!</Text>
-      <Text>This is Development Branch</Text>
-      <Text>This is Rezaul Karim Branch</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+         <Stack.Navigator>
+           <Stack.Screen name="Login" component={LoginScreen}/>
+         </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
