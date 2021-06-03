f5c7c77fb1b03728dedf38e0085f7c385384e6c6
Abid Nuri <mdabidnuri@gmail.com>
2021-06-03 19:18:18 +0600

    from abid

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>From Abid Nuri</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
