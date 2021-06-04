a0416f82ef8d96081df2a7013ff0e6ea495bb135
Abid Nuri <mdabidnuri@gmail.com>
2021-06-04 11:57:52 +0600

    signout function & photourl show

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { View, Text, Touchable } from 'react-native'
+import { auth } from '../firebase';
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation }) => {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <View style={{
+                    marginLeft: 20,
+                }}>
+                    <Avatar
+                        rounded
+                        source={{
+                            uri: auth?.currentUser?.photoURL
+                        }}
+                    />
+                </View>
+            ),
+            headerRight: () => (
+                <TouchableOpacity style={{
+                    marginRight: 30,
+                }}
+                    onPress={signOut}
+                >
+                    <AntDesign name="logout" size={24} color="black" />
+                </TouchableOpacity>
+            )
+        })
+    }, [])
+    const signOut = () => {
+        auth.signOut().then(() => {
+            // Sign-out successful.
+            navigation.replace('Login')
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
     return (
         <View>
             <Text>Chat Screen</Text>
