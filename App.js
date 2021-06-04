66046eb3ea092119270ba9f7cbf80bd209e65431
shahin1122 <passportscotch7@gmail.com>
2021-06-04 12:18:51 +0600

    added new text

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Shahin Shah</Text>
+      <Text>Added new </Text>
       <StatusBar style="auto" />
     </View>
   );
