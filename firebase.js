38256e954937eb91a0a468f8698529d404109de0
Abid Nuri <mdabidnuri@gmail.com>
2021-06-04 09:36:34 +0600

    npm install problem

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVD5JYm12jV7-TjVzW4vjbh3aieHMgChQ",
+    authDomain: "dev-chat-58672.firebaseapp.com",
+    projectId: "dev-chat-58672",
+    storageBucket: "dev-chat-58672.appspot.com",
+    messagingSenderId: "211667763536",
+    appId: "1:211667763536:web:17fe8c9858c8b962cdec55",
+    measurementId: "G-RZC7BZSH7Z"
+  };
