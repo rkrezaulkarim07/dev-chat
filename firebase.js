f006d5ad8ac546f435b65d7a81d61f706006d7f7
Abid Nuri <mdabidnuri@gmail.com>
2021-06-04 10:28:10 +0600

    firebase import error solved & config

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDVD5JYm12jV7-TjVzW4vjbh3aieHMgChQ",
     authDomain: "dev-chat-58672.firebaseapp.com",
@@ -7,3 +11,14 @@ const firebaseConfig = {
     appId: "1:211667763536:web:17fe8c9858c8b962cdec55",
     measurementId: "G-RZC7BZSH7Z"
   };
+
+  let app;
+  if(firebase.apps.length === 0){
+      app = firebase.initializeApp(firebaseConfig);
+  } else{
+      app = firebase.app()
+  }
+  const db = app.firestore();
+  const auth = firebase.auth();
+  export {db, auth};
+  export default firebaseConfig;
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Input, Button } from 'react-native-elements';
+import { auth } from '../firebase';
 
 const RegisterScreen = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
-    const [imageURl, setImageUrl] = useState('');
+    const [imageURL, setImageUrL] = useState('');
+
+    const register = () => {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+
+                user.updateProfile({
+                    displayName: name,
+                    photoURL: imageURL ? imageURL : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+                }).then(function () {
+                    // Update successful.
+                }).catch(function (error) {
+                    // An error happened.
+                });
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                alert(errorMessage)
+            });
+    }
+
     return (
         <View style={styles.container}>
             <Input
@@ -35,23 +58,23 @@ const RegisterScreen = () => {
                 placeholder="Enter your image Url"
                 label="Profile Picture"
                 leftIcon={{ type: 'material', name: 'face' }}
-                value={imageURl}
-                onChange={text => setImageUrl(text)}
+                value={imageURL}
+                onChange={text => setImageUrL(text)}
             />
-            
+
             <Button class="registerButton" title="Register" style={styles.button} />
         </View>
     )
 }
 
-export default RegisterScreen 
+export default RegisterScreen
 
 const styles = StyleSheet.create({
     button: {
         width: 200,
         marginTop: 10,
-        
-        
+
+
     },
 
     container: {
